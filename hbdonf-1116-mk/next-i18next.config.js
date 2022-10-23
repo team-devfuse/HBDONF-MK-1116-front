87768b2bb9354c9157f2c834e1446ebcd6171356
ES, Choi <69956156+escape1001@users.noreply.github.com>
2022-10-24 00:36:08 +0900

    Update next-i18next.config.js

--- hbdonf-1116-mk/next-i18next.config.js
+++ hbdonf-1116-mk/next-i18next.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'ko',
     locales: ['ko', 'en', 'jp'],
     // localeDetection: false,
     // lowerCaseLng: true,
+    localePath: path.resolve('./public/locales')
   },
 };
