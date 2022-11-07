d4f1e567d7119208310de9df62c8279ec740da34
ES, Choi <69956156+escape1001@users.noreply.github.com>
2022-11-08 01:06:47 +0900

    "update" 메세지 목업 api 수정

--- hbdonf-1116-mk/lib/config.js
+++ hbdonf-1116-mk/lib/config.js
@@ -1,3 +1,5 @@
-export const API_URL = 'https://cors-anywhere.herokuapp.com/http://44.241.252.158:8081/mk';
+// export const API_URL = 'https://hbdonf.team-devfuse.com/mk';
+// export const API_URL = 'https://cors-anywhere.herokuapp.com/https://hbdonf.team-devfuse.com/mk';
+export const API_URL = '/api';
 
 export const S3_BASE_URL = 'https://d3bjhg9thms238.cloudfront.net';
