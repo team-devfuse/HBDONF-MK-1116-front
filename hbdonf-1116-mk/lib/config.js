386d8d6cd1121cc4243fb5894617ed1ce5f10d56
ES, Choi <69956156+escape1001@users.noreply.github.com>
2022-11-12 11:14:12 +0900

    API 도메인 수정

--- hbdonf-1116-mk/lib/config.js
+++ hbdonf-1116-mk/lib/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'https://hbdonf.team-devfuse.com/mk';
+export const API_URL = 'https://hbdonf-api.team-devfuse.com/mk';
 
 export const S3_BASE_URL = 'https://d3bjhg9thms238.cloudfront.net';
