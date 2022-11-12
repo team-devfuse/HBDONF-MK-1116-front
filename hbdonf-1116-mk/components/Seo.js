79a8573ee9d7321851f32a350055a3c3b889b95c
ES, Choi <69956156+escape1001@users.noreply.github.com>
2022-11-12 17:06:45 +0900

    메타태그, 파비콘 수정

--- hbdonf-1116-mk/components/Seo.js
+++ hbdonf-1116-mk/components/Seo.js
@@ -9,7 +9,7 @@ export default function Seo(){
             <meta property="og:description" content="yoyo 소리질러~!! 말풍선을 획득해 11월 16일 생일을 맞은 온앤오프 MK의 생일을 축하해보세요!"/>
             <meta property="og:image" content="https://d3bjhg9thms238.cloudfront.net/mk_1116/meta_hbdonf_mk.png"/>
             <meta property="og:image:alt" content="[HBDONF-MK]1116 Soriziller Festival"/>
-            <meta name="twitter:card" content="summary"/>
+            <meta name="twitter:card" content="summary_large_image"/>
             {/* <meta name="twitter:card" content="summary_large_image"/> */}
             <meta name="twitter:description" content="yoyo 소리질러~!! 말풍선을 획득해 11월 16일 생일을 맞은 온앤오프 MK의 생일을 축하해보세요!"/>
             <meta name="twitter:image" content="https://d3bjhg9thms238.cloudfront.net/mk_1116/meta_hbdonf_mk.png"/>
